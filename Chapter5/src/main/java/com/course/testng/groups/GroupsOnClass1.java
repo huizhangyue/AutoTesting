7074d217ef28af76e92f627a50746872d0fd87ed
huizhangyue <15011269379@163.com>
2020-04-06 15:54:57 +0800

    基础测试，提交代码

--- Chapter5/src/main/java/com/course/testng/groups/GroupsOnClass1.java
+++ Chapter5/src/main/java/com/course/testng/groups/GroupsOnClass1.java
@@ -1,4 +1,10 @@
 package com.course.testng.groups;
 
+import org.testng.annotations.Test;
+
+@Test
 public class GroupsOnClass1 {
+    public void stu1(){
+        System.out.println("");
+    }
 }
--- Chapter5/src/main/java/com/course/testng/ignoreTest.java
+++ Chapter5/src/main/java/com/course/testng/ignoreTest.java
@@ -1,4 +1,18 @@
 package com.course.testng;
 
+import org.testng.annotations.Test;
+
 public class ignoreTest {
+    @Test
+    public void ignore1(){
+        System.out.println("ignore 1  执行");
+    }
+    @Test(enabled = false)
+    public void ignore2(){
+        System.out.println("ignore 2 执行");
+    }
+    @Test(enabled = true)
+    public void ignore3(){
+        System.out.println("ignore3 执行");
+    }
 }
