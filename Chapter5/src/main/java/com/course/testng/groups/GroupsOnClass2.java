c4e102594296cccab742a437e537724ff3dfb979
huizhangyue <15011269379@163.com>
2020-04-19 20:02:19 +0800

    Chapter5提交测试

--- Chapter5/src/main/java/com/course/testng/groups/GroupsOnClass2.java
+++ Chapter5/src/main/java/com/course/testng/groups/GroupsOnClass2.java
@@ -1,4 +1,13 @@
 package com.course.testng.groups;
 
+import org.testng.annotations.Test;
+
+@Test(groups = "teacher")
 public class GroupsOnClass2 {
+    public void teacher1(){
+        System.out.println("GroupsOnClass1中的teacher1111运行");
+    }
+    public void teacher2(){
+        System.out.println("GroupsOnClass1中的teacher2222运行");
+    }
 }
--- Chapter5/src/main/java/com/course/testng/groups/GroupsOnClass3.java
+++ Chapter5/src/main/java/com/course/testng/groups/GroupsOnClass3.java
@@ -1,4 +1,13 @@
 package com.course.testng.groups;
 
+import org.testng.annotations.Test;
+
+@Test
 public class GroupsOnClass3 {
+    public void teacher1(){
+        System.out.println("GroupsOnClass1中的teacher1111运行");
+    }
+    public void teacher2(){
+        System.out.println("GroupsOnClass1中的teacher2222运行");
+    }
 }
